57e8162ccc5a3097d111cccdf7ac0121e3cf5954
chaithra-harshi <chaithrabn96@gmail.com>
2020-04-04 20:01:32 +0530

    chaithra adding springValueprofilefile implementation

---
+++ SpringValueFromProfilefile/src/main/java/com/bridgelabz/loadingpropfromfile/valuefield/Client.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.loadingpropfromfile.valuefield;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/* *  Purpose: Program is written for dependency injection of spring using
+ *  		 by value field with declaration injection configure in xml
+ *  		 and in this loading property from the student_info.properties file
+ *
+ */
+public class Client {
+
+	public static void main(String[] args) {
+
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Student student = context.getBean("stud", Student.class);
+		student.displayStudentInfo();
+
+	}
+
+}
---
+++ SpringValueFromProfilefile/src/test/java/com/bridgelabz/loadingpropfromfile/valuefield/SpringValueFromProfilefileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.loadingpropfromfile.valuefield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValueFromProfilefileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
