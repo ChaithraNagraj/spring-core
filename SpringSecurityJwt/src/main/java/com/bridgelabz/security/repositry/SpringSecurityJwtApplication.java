0ca5501e9868daa31728bc814a56590d66182deb
chaithraNagraj <56997768+chaithra-harshi@users.noreply.github.com>
2020-04-04 16:39:57 +0530

    Update SpringSecurityJwtApplication.java
    done

--- SpringSecurityJwt/src/main/java/com/bridgelabz/security/repositry/SpringSecurityJwtApplication.java
+++ SpringSecurityJwt/src/main/java/com/bridgelabz/security/repositry/SpringSecurityJwtApplication.java
@@ -1,5 +1,5 @@
 package com.bridgelabz.security.repositry;
-
+//main method 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
