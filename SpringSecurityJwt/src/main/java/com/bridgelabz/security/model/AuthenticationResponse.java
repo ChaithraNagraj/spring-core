5f091593e8fca403ddabbfdc6ae66d7f9202bc5b
chaithraNagraj <56997768+chaithra-harshi@users.noreply.github.com>
2020-04-05 17:40:02 +0530

    Update AuthenticationResponse.java
    changes

--- SpringSecurityJwt/src/main/java/com/bridgelabz/security/model/AuthenticationResponse.java
+++ SpringSecurityJwt/src/main/java/com/bridgelabz/security/model/AuthenticationResponse.java
@@ -4,31 +4,12 @@ import java.io.Serializable;
 
 public class AuthenticationResponse  implements Serializable{
 
-	/**package com.bridgelabz.security.model;
-
-import java.io.Serializable;
-
-public class AuthenticationResponse  implements Serializable{
-
 	/**
 	 * output structure
 	 * here bwe are creating jw web token
 	 * 
 	 * this will invoke when a proper request get launched
 	 * means to send a proper response to client
-	 */
-	private static final long serialVersionUID=1L;
-	
-	private final String jwt;
-	public AuthenticationResponse(String jwt) {
-		this.jwt=jwt;
-	}
-	public String getJwt() {
-		return jwt;
-	}
-}
-
-	 * 
 	 */
 	private static final long serialVersionUID=1L;
 	
