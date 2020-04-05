3c49966332018f20cc02d1030cf4f82542c21382
chaithraNagraj <56997768+chaithra-harshi@users.noreply.github.com>
2020-04-05 17:38:17 +0530

    Update AuthenticationRequest.java
    changes done

--- SpringSecurityJwt/src/main/java/com/bridgelabz/security/model/AuthenticationRequest.java
+++ SpringSecurityJwt/src/main/java/com/bridgelabz/security/model/AuthenticationRequest.java
@@ -3,6 +3,43 @@ package com.bridgelabz.security.model;
 import java.io.Serializable;
 
 public class AuthenticationRequest  implements Serializable{
+package com.bridgelabz.security.model;
+
+import java.io.Serializable;
+
+public class AuthenticationRequest  implements Serializable{
+
+	/**Authentication request checking info provided here
+	 * its going to defined the input auth
+	 */
+	private static final long serialVersionUID=1l;
+	private String username;
+	private String password;
+	public String getUsername() {
+		return username;
+		}
+	
+	public void setUsername(String username) {
+		this.username=username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password=password;
+	}
+	//we need default constructor for json pasing
+
+	public AuthenticationRequest() {
+		
+	}
+	public AuthenticationRequest(String username,String password) {
+		this.setUsername(username);
+		this.setPassword(password);
+	}
+	
+}
 
 	/**
 	 * 
