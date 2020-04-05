e996df16298f8570a5dba25eda19d869a9048c6d
chaithraNagraj <56997768+chaithra-harshi@users.noreply.github.com>
2020-04-05 17:43:16 +0530

    Update SecurityConfigurer.java
    changes

--- SpringSecurityJwt/src/main/java/com/bridgelabz/security/repositry/SecurityConfigurer.java
+++ SpringSecurityJwt/src/main/java/com/bridgelabz/security/repositry/SecurityConfigurer.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.security.repositry;
+package com.bridgelabz.security.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,14 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import com.bridgelabz.security.filters.JwtRequestFilter;
 import com.bridgelabz.security.services.MyUserDetailsService;
 
 @SuppressWarnings("deprecation")
+
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 	@Autowired
@@ -28,7 +28,8 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(myUserDetailsSerive);
 	}
-
+//below method authorises request 
+	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable()
 				.authorizeRequests().antMatchers("/auth").permitAll().
@@ -39,6 +40,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 	}
 	
+	@Override
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
