9e922dcd904a1478fc1a348330b9428b481fc4c5
chaithraNagraj <56997768+chaithra-harshi@users.noreply.github.com>
2020-04-05 17:44:08 +0530

    Update SpringSecurityJwtApplication.java
    work from home things

--- SpringSecurityJwt/src/main/java/com/bridgelabz/security/repositry/SpringSecurityJwtApplication.java
+++ SpringSecurityJwt/src/main/java/com/bridgelabz/security/repositry/SpringSecurityJwtApplication.java
@@ -1,12 +1,19 @@
-package com.bridgelabz.security.repositry;
-//main method 
+package com.bridgelabz.security.security;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages="com.bridgelabz.security.filters,com.bridgelabz.security.services,com.bridgelabz.security.util")
 public class SpringSecurityJwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
 	}
 }
+/**
+****purpose of this application
+* 1. create a new authentication APi enpoint
+* 2.examine every incoming request for valid jwt and authorize
+*/
